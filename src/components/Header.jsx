3a82706b202058074ae58948cb491065d5ede314
Artem Glazyrin <royalbeggars@yandex.ru>
2021-04-14 22:43:54 +0500

    project init, v.1.0.0

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,22 +6,17 @@ function Header() {
     return (
         <div className="header">
             <div className="container">
-                <a className="link">
-                    <div className="header__logo">
-                        <img src="" alt=""/>
-                        <div>
-                            <h1>React Catalogue</h1>
-                            <p>почти то же, что на Vue</p>
-                        </div>
+                <div className="header__logo">
+                    <img src="" alt="" />
+                    <div>
+                        <h1>React Catalogue</h1>
+                        <p>почти то же, что на Vue</p>
                     </div>
-                </a>
-
+                </div>
                 <div className="header__cart">
-                    <a href="" className="link">
-                        <button>
+                    <button>
+                    </button>
 
-                        </button>
-                    </a>
                 </div>
             </div>
         </div>
